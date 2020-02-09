c280a08cad2c8f5a451c821887fb94813839e43d
ArnoutKroeze <Arnoutkroeze@gmail.com>
2020-02-09 14:59:24 +0100

    update database

--- dbhelper.py
+++ dbhelper.py
@@ -1,6 +1,5 @@
-from sqlalchemy import create_engine, func
+from sqlalchemy import create_engine, func, select
 from sqlalchemy.orm import sessionmaker
-
 from base import Base, Games
 
 
@@ -15,21 +14,48 @@ class DBHelper:
         Base.metadata.create_all(self.engine)
 
     def get_max_game_id(self):
-        print(self.session.querry(func.max(games.game_id)).first())
-        pass
+        return self.session.query(func.max(Games.game_id)).first()[0]
 
 
-    def new_game(self, date):
-        game = Games(game_id = 2, player_1 = 'arnout',player_2 =  'Japser',player_3 = 'anna',player_4 =  'Peter', score_1 = 0,score_2 = 0, date = date)
+    def new_game(self, player_1, player_2, player_3, player_4, date):
+        game = Games(game_id = self.get_max_game_id + 1, player_1 = player_1, player_2 =  player_2, player_3 = player_3, player_4 =  player_4, score_1 = 0,score_2 = 0, date = date)
         self.session.add(game)
         self.session.commit()
 
-    def add_score(self, score1, score2):
-        pass
+    def add_score(self, score1, score2, date):
+        scores = self.session.query(Games).filter_by(game_id = self.get_max_game_id()).first()
+        new_scores = Games(game_id = scores.game_id, player_1 = scores.player_1, player_2 = scores.player_2,
+                    player_3 = scores.player_3, player_4 = scores.player_4, score_1 = score1, score_2 = score2,
+                    date = date)
+        self.session.add(new_scores)
+        self.session.commit()
     
+    def current_score(self):
+        games = self.session.query(Games).filter_by(game_id = self.get_max_game_id())
+        total_1 = 0
+        total_2 = 0
+        for game in games:
+            total_1 += game.score_1
+            total_2 += game.score_2
+        return f"{total_1}, {total_2}"
+
+    def scoreverloop(self):
+        games = (self.session.query(Games)
+                .filter_by(game_id = self.get_max_game_id())
+                .order_by(Games.date))
+
+        verloop = ""
+        verloop += "`" + games[0].player_1 + "".ljust(10-len(games[0].player_1)) + games[0].player_3 + "`\n"
+        verloop += "`" + games[0].player_2 + "".ljust(10-len(games[0].player_2)) + games[0].player_4 + "`\n"
+
+        team1 = 0
+        team2 = 0
+        for game in games:
+            team1 += game.score_1
+            team2 += game.score_2
+            verloop += f"`{team1}" + "".ljust(10 - len(str(team1))) + f"{team2}`\n"
+        return verloop
 db = DBHelper()
 if __name__ == "__main__":
-    db.new_game('bier')
-    db.get_max_game_id()
-    
+    db.current_score()
     print('taipan!')
--- telegrambot.py
+++ telegrambot.py
@@ -4,7 +4,7 @@ import configparser as cfg
 import traceback
 import os
 import sys
-
+import datetime
 from dbhelper import DBHelper
 
 
@@ -156,11 +156,11 @@ Geeft het scoreverloop van dit spel weer'''
             self.send_message('Dit is samen geen honderdtal hé', chat)
             return 
         elif score1 % 5:
-            self.send_message('wtf hoe kom je aan {} punten'.format(5 - (score1 % 5)), chat)
+            self.send_message('wtf hoe kom je aan {} punten'.format((score1 % 5)), chat)
             return
         try: 
             self.add_score(score1, score2)
-            update_message = "Het staat nu {}".format(self.get_current_score())
+            update_message = "Het staat nu {}".format(self.db.current_score())
             self.send_message(update_message, chat)
             return
         except Exception as e:
@@ -172,10 +172,7 @@ Geeft het scoreverloop van dit spel weer'''
         switcher = {
             '/help': self.show_help,
             '/start': self.start_game,
-            '/user': self.add_user,         
             '/vera': self.vera,
-            '/team1': self.team_one,
-            '/team2': self.team_two,
             '/verloop': self.show_scoreverloop
         }
         split_text = text.split()
@@ -192,70 +189,17 @@ Geeft het scoreverloop van dit spel weer'''
         self.send_message(message, chat)
 
     def start_game(self, chat, text, telegram_id, name):
-        data = {
-            "Team1":{
-                "Player1": "null",
-                "Player2": "null",
-                "Scoreverloop": [0]                   
-            },
-            "Team2":{
-                "Player1": "null",
-                "Player2": "null",
-                "Scoreverloop": [0]
-            }
-        }
-        with open(self.data_file, "w") as testfile:
-            json.dump(data, testfile)
+        split_text = text.split()
+        if len(split_text) != 5:
+            self.send_message('Voer 4 namen in', chat)
+            return
+        if max(split_text, key=len) > 10:
+            self.send_message('Namen hoeven echt niet langer dan 10 characters te zijn', chat)
+
+        self.db.new_game(split_text[1], split_text[2], split_text[3], split_text[4], datetime.datetime.now())
+
         message = "We zijn een nieuw spelletje begonnen!"
         self.send_message(message, chat)
-        self.db.new_game('lalala')
-        return
-
-    def team_one(self, chat, text, telegram_id, name):
-        if len(text) != 3:
-            self.send_message("Voer twee namen in", chat)
-            return
-
-        if len(text[1]) > 11 or len(text[2]) > 11:
-            self.send_message('Max 10 letters per naam', chat)
-            return
-
-        with open(self.data_file, 'r') as file:
-            print(text)
-            data = json.load(file)
-            print(data)
-
-
-
-        data["Team1"]["Player1"] = text[1]
-        data["Team1"]["Player2"] = text[2]
-
-        with open(self.data_file,'w') as file:
-            json.dump(data, file)
-        self.send_message("ging goed", chat)
-        return
-
-    def team_two(self, chat, text, telegram_id, name):
-        if len(text) != 3:
-            self.send_message("Voer twee namen in", chat)
-            return
-
-        if len(text[1]) > 11 or len(text[2]) > 11:
-            self.send_message('Max 10 letters per naam', chat)
-            return
-
-        with open(self.data_file, 'r') as file:
-            print(text)
-            data = json.load(file)
-            print(data)
-
-        print(text[1])
-        data["Team2"]["Player1"] = text[1]
-        data["Team2"]["Player2"] = text[2]
-
-        with open(self.data_file,'w') as file:
-            json.dump(data, file)
-        self.send_message("ging goed", chat)
         return
     
     def vera(self, chat, text, telegram_id, name):
@@ -263,57 +207,15 @@ Geeft het scoreverloop van dit spel weer'''
         self.send_message(message, chat)
         return
 
-    def add_user(self, chat, text, telegram_id, name):
-        self.send_message('uuh', chat)
-        return
-
     def show_scoreverloop(self, chat, text, telegram_id, name):
-
-        with open(self.data_file, 'r') as file:
-            data = json.load(file)
-        try:
-            scores_team1 = data["Team1"]["Scoreverloop"]
-            scores_team2 = data["Team2"]["Scoreverloop"]
-            player1team1 = data["Team1"]["Player1"]
-            player2team1 = data["Team1"]["Player2"]
-            player1team2 = data["Team2"]["Player1"]
-            player2team2 = data["Team2"]["Player2"]
-            message = ""
-            print(len(player1team1))
-            # TODO make this prettier
-            message += "`" + player1team1 + "".ljust(10-len(player1team1)) + player1team2 + "`\n"
-            message += "`" + player2team1 + "".ljust(10-len(player2team1)) + player2team2 + "`\n"
-
-            for line in range(len(scores_team1)):
-                print('len is: {}'.format(len(str(scores_team1[line]))))
-                message += "`{}".format(scores_team1[line]) + "".ljust(10-len(str(scores_team1[line]))).format(scores_team1[line]) + "{}`\n".format(scores_team2[line])
-            self.send_message(message, chat, 'MarkdownV2')
-            return
-        except Exception as e:
-            print(e)
-            return
+        message = self.db.scoreverloop()
+        self.send_message(message, chat, 'MarkdownV2')
+        pass
 
     def add_score(self, points1: int, points2: int):
-
-        #input_scores(((self.current_game, 2, 3, points1, points2, 'probably at tuesday')))
-
-
-
-        with open(self.data_file, "r") as testfile:
-            scores = json.load(testfile)
-            scores["Team1"]["Scoreverloop"] += [scores["Team1"]["Scoreverloop"][-1] + points1]
-            scores["Team2"]["Scoreverloop"] += [scores["Team2"]["Scoreverloop"][-1] + points2]
-        with open(self.data_file, "w") as testfile:
-            json.dump(scores, testfile)
+        self.db.add_score(points1, points2, datetime.datetime.now())
         return
 
-    def get_current_score(self):
-        with open (self.data_file, 'r') as file:
-            data = json.load(file)
-            punten_team1 = data["Team1"]["Scoreverloop"][-1]
-            punten_team2 = data["Team2"]["Scoreverloop"][-1]
-            return "{} {}".format(punten_team1, punten_team2)
-        return
 
 if __name__ == '__main__':
     daniel = telegram_chatbot()
